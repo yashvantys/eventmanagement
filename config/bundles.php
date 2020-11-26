1cbc1fd7951af08729d83afc27f0787d9947c2db
Yashvant Yadav <yashvant.yadav@kitelytech.com>
2020-11-26 10:57:24 +0530

    Initial commit

---
+++ config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+];
